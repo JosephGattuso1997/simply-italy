de0f32236d8f844a96a0b169161c327d0164e8c5
Joseph Gattuso <31634907+JosephGattuso1997@users.noreply.github.com>
2021-07-30 14:43:12 -0400

    git

--- blog/settings.py
+++ blog/settings.py
@@ -1,6 +1,5 @@
 import os
 import django_heroku
-from boto.s3.connection import S3Connection
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
