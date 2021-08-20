51c2c508dcd9fdc7e91da61fd9beca2cd286bed5
Joseph Gattuso <31634907+JosephGattuso1997@users.noreply.github.com>
2021-08-20 09:35:13 -0400

    gallery and map

--- blog/settings.py
+++ blog/settings.py
@@ -1,6 +1,8 @@
 import os
 import django_heroku
 
+
+LOGIN_REDIRECT_URL = 'post-list'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
